fe4d7f63a7ab1f2b56c95a031ef817a4e8c4413f
Terem <staskov2009@mail.ru>
2019-06-20 07:27:17 +0300

    '20.06.2019_07.27'

---
+++ commit.bat
@@ -0,0 +1,24 @@
+set dd=%DATE%
+set tt=%TIME%
+
+set /a ddd=%dd:~0,2%
+IF %ddd% LSS 10 (
+  SET day=0%ddd%) else (
+  SET day=%ddd%)
+set month=%dd:~3,2%
+set year=%dd:~6,4%
+
+set /a ttt=%tt:~0,2%
+
+IF %ttt% LSS 10 (
+  SET hour=0%ttt%) else (
+  SET hour=%ttt%)
+
+SET minute=%tt:~3,2%
+SET sec=%tt:~6,2%
+set var1=%day%.%month%.%year%_%hour%.%minute%
+
+git init
+git add .
+git commit -m '%var1%'
+git push https://github.com/Tremorok/NorthGodAHK master
--- compill.bat
+++ compill.bat
@@ -1,12 +1,32 @@
 del final /q
+del latestversion /q
 mkdir final
+mkdir final\images
+mkdir final\extensions
+mkdir latestversion
+mkdir latestversion\extensions
+mkdir latestversion\images
 mkdir final\BackupsExe
-for %%a in (*.ahk) do if "%%a"=="main.ahk" (Compress\Ahk2Exe.exe /in %%a /icon easylife.ico) else (Compress\Ahk2Exe.exe /in %%a)
+for %%a in (*.ahk) do if "%%a"=="main.ahk" (Compress\Ahk2Exe.exe /in %%a /icon NGAHK.ico) else (Compress\Ahk2Exe.exe /in %%a)
+for %%a in (extensions\*.ahk) do (Compress\Ahk2Exe.exe /in %%a)
 copy *.exe final\BackupsExe
+copy extensions\*.exe final\BackupsExe\extensions
 for %%a in (*.exe) do Compress\mpress.exe %%a
+for %%a in (extensions\*.exe) do Compress\mpress.exe %%a
 move *.exe final
+move extensions\*.exe final\extensions
+copy final\extensions\*.exe latestversion\extensions
 copy config.ini final
-
+copy images\*.* final\images
+copy images\*.* latestversion\images
+copy changelog.txt final
+copy changelog.txt latestversion
+copy images latestversion
+copy final\*.* latestversion
+copy final\changelog.txt
+copy latestversion\changelog.txt
+copy update.bat final\update.bat
+copy update.bat latestversion\update.bat
 rem @echo off
 
 set dd=%DATE%
@@ -27,9 +47,11 @@ IF %ttt% LSS 10 (
 
 SET minute=%tt:~3,2%
 SET sec=%tt:~6,2%
-
+set var1=%day%.%month%.%year%_%hour%.%minute%
 cd final
-"%programfiles%\WinRar\Winrar.exe" A %day%.%month%.%year%_%hour%.%minute%.rar *.*
+"..\Compress\7z.exe" A %var1%.zip *.*
+"..\Compress\7z.exe" A %var1%.zip images\*.*
+"..\Compress\7z.exe" A %var1%.zip extensions\*.*
 cd ..\
 mkdir release
-move final\*.rar release
+move final\*.zip release
