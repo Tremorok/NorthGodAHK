231bd6083487b74bdd507e84bda485f1d942153c
Terem <staskov2009@mail.ru>
2019-06-21 12:41:38 +0300

    21.06.2019

---
+++ compill.bat
@@ -0,0 +1,49 @@
+del final /q
+mkdir final
+mkdir final\images
+mkdir final\extensions
+mkdir final\BackupsExe
+mkdir final\BackupsExe\extensions
+for %%a in (*.ahk) do if "%%a"=="main.ahk" (Compress\Ahk2Exe.exe /in %%a /icon NGAHK.ico) else (Compress\Ahk2Exe.exe /in %%a)
+for %%a in (extensions\*.ahk) do (Compress\Ahk2Exe.exe /in %%a)
+copy *.exe final\BackupsExe
+copy extensions\*.exe final\BackupsExe\extensions
+for %%a in (*.exe) do Compress\mpress.exe %%a
+for %%a in (extensions\*.exe) do Compress\mpress.exe %%a
+move *.exe final
+move extensions\*.exe final\extensions
+copy config.ini final
+copy images\*.* final\images
+copy changelog.txt final
+copy final\changelog.txt
+copy update.bat final\update.bat
+rem @echo off
+
+set dd=%DATE%
+set tt=%TIME%
+
+set /a ddd=%dd:~0,2%
+IF %ddd% LSS 10 (
+  SET day=0%ddd%) else (
+  SET day=%ddd%)
+set month=%dd:~3,2%
+set year=%dd:~6,4%
+
+set /a ttt=%tt:~0,2%
+
+IF %ttt% LSS 10 (
+  SET hour=0%ttt%) else (
+  SET hour=%ttt%)
+
+SET minute=%tt:~3,2%
+SET sec=%tt:~6,2%
+set var1=%day%.%month%.%year%_%hour%.%minute%
+cd final
+"..\Compress\7z.exe" A %var1%.zip *.*
+"..\Compress\7z.exe" A %var1%.zip images\*.*
+"..\Compress\7z.exe" A %var1%.zip extensions\*.*
+cd ..\
+mkdir release
+move final\*.zip release
+copy release\%var1%.zip final
+ren final\%var1%.zip latestversion.zip
